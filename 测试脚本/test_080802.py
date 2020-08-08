854d80e6ceafefac5643100cf4367a3132751551
孟鑫 <907008127@qq.com>
2020-08-08 23:54:45 +0800

    test

--- 测试脚本/test_080802.py
+++ 测试脚本/test_080802.py
@@ -13,7 +13,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 class Test080802(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Firefox()
+        self.driver = webdriver.Firefox();
         self.vars = {}
 
     def tearDown(self):
@@ -22,8 +22,10 @@ class Test080802(unittest.TestCase):
     def test_080802(self):
         self.driver.get("https://www.baidu.com/")
         self.driver.set_window_size(1037, 829)
+        # 用name来定位
         self.driver.find_element(By.ID, "kw").send_keys("守望先锋")
         self.driver.find_element(By.ID, "su").click()
+
         time.sleep(6)
 
 
