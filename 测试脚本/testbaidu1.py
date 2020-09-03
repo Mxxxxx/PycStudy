51d902c1b84748672616e16a4f8c39b91eaf91d8
孟鑫 <907008127@qq.com>
2020-09-03 22:13:47 +0800

    tj

--- 测试脚本/testbaidu1.py
+++ 测试脚本/testbaidu1.py
@@ -21,10 +21,11 @@ class Baidu1(unittest.TestCase):
         self.base_url = "http://www.baidu.com/"
         self.verificationErros = []
         self.accept_next_alert = True
+
     # 数据驱动使用
-    @unittest.skip("skipping")
-    @data("你好","守望")
-    def test_baidusearch(self,value):
+
+    @data("你好", "守望")
+    def test_baidusearch(self, value):
         driver = self.driver
         driver.get(self.base_url + "/")
         driver.find_element_by_id("kw").clear()
@@ -48,50 +49,48 @@ class Baidu1(unittest.TestCase):
         try:
             self.assertEqual(u"hao123_上网从这里开始码", driver.title)
         except:
-            self.savescreenshot(driver,"hao123Error.png")
+            self.savescreenshot(driver, "hao123Error.png")
         time.sleep(6)
         driver.quit()
 
     def savescreenshot(self, driver, file_name):
         if not os.path.exists('./errorImage'):
             os.makedirs("./errorImage")
-        now=time.strftime("%Y%m%d-%H%M%S",time.localtime(time.time()))
-        driver.get_screenshot_as_file("./errorImage/"+now+file_name)
+        now = time.strftime("%Y%m%d-%H%M%S", time.localtime(time.time()))
+        driver.get_screenshot_as_file("./errorImage/" + now + file_name)
         time.sleep(3)
 
+    def is_element_present(self, how, what):
+        try:
+            self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e:
+            return False
+        return True
 
-def is_element_present(self, how, what):
-    try:
-        self.driver.find_element(by=how, value=what)
-    except NoSuchElementException as e:
-        return False
-    return True
+    # 判断alert是否存在，可删除
+    def is_alert_present(self):
+        try:
+            self.driver.switch_to_alert()
+        except NoAlertPresentException as e:
+            return False
+        return True
 
+    # 关闭alert，可删除
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally:
+            self.accept_next_alert = True
 
-# 判断alert是否存在，可删除
-def is_alert_present(self):
-    try:
-        self.driver.switch_to_alert()
-    except NoAlertPresentException as e:
-        return False
-    return True
+        # test fixture，清除环境
+        # 测试固件
 
-
-# 关闭alert，可删除
-def close_alert_and_get_its_text(self):
-    try:
-        alert = self.driver.switch_to_alert()
-        alert_text = alert.text
-        if self.accept_next_alert:
-            alert.accept()
-        else:
-            alert.dismiss()
-        return alert_text
-    finally:
-        self.accept_next_alert = True
-
-    # test fixture，清除环境
-    # 测试固件
     def tearDown(self):
         self.driver.quit()
         self.assertEqual([], self.verificationErrors)
--- 测试脚本/testbaidu2.py
+++ 测试脚本/testbaidu2.py
@@ -4,6 +4,7 @@ import sys
 
 from ddt import ddt, data, unpack, file_data
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
@@ -20,22 +21,9 @@ class Baidu1(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://www.baidu.com/"
+        self.base_url = "http://localhost:8080/sis/public/index.html"
         self.verificationErros = []
         self.accept_next_alert = True
-
-    def getText(file_name):
-        rows = []
-        path = sys.path[0]
-        with open(path + '/data/' + file_name, 'rt') as f:
-            readers = csv.reader(f, delimiter=',', quotechar='|')
-            next(texts, None)
-            for row in readers:
-                temprows = []
-                for i in row:
-                    temprows.append(i)
-                rows.append(temprows)
-            return rows
 
     # 数据驱动
     # @data(["你好", u"你好_百度搜索"], ["守望", u"守望啊_百度"])
@@ -45,23 +33,91 @@ class Baidu1(unittest.TestCase):
     # @file_data('testjson.json')
 
     # 文件读取
-    @data(*getText('test_baidu.txt'))
+    # @data(*getText('test_baidu.txt'))
     @unpack
-    def test_baidusearch(self, value,value1):
-        driver = self.driver
-        driver.get(self.base_url + "/")
-        driver.find_element_by_id("kw").clear()
-        driver.find_element_by_id("kw").send_keys(value)
-        driver.find_element_by_id("su").click()
-
-        # 断言测试
-        time.sleep(6)
-        # try:
-        self.assertEqual( driver.title,value1, msg="内容不一致")
-        # except:
-        #     self.savescreenshot(driver,"baiduerror.png")
-        time.sleep(3)
-        driver.quit()
+    def test_baidusearch(self):
+        self.driver.get(self.base_url+'/')
+        self.driver.set_window_size(1488, 878)
+        self.driver.find_element(By.NAME, "username").click()
+        self.driver.find_element(By.NAME, "username").click()
+        element = self.driver.find_element(By.NAME, "username")
+        actions = ActionChains(self.driver)
+        actions.double_click(element).perform()
+        self.driver.find_element(By.ID, "login_container").click()
+        self.driver.find_element(By.NAME, "password").click()
+        self.driver.find_element(By.NAME, "password").click()
+        element = self.driver.find_element(By.NAME, "password")
+        actions = ActionChains(self.driver)
+        actions.double_click(element).perform()
+        self.driver.find_element(By.NAME, "password").send_keys("123456")
+        self.driver.find_element(By.CSS_SELECTOR, "#login_form .btn-success").click()
+        self.driver.find_element(By.NAME, "username").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".mt-2:nth-child(5)").click()
+        self.driver.find_element(By.NAME, "password").click()
+        self.driver.find_element(By.NAME, "password").click()
+        element = self.driver.find_element(By.NAME, "password")
+        actions = ActionChains(self.driver)
+        actions.double_click(element).perform()
+        self.driver.find_element(By.NAME, "password").click()
+        self.driver.find_element(By.NAME, "password").click()
+        element = self.driver.find_element(By.NAME, "password")
+        actions = ActionChains(self.driver)
+        actions.double_click(element).perform()
+        self.driver.find_element(By.NAME, "password").send_keys("123")
+        self.driver.find_element(By.CSS_SELECTOR, "#login_form .btn-success").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".mr-auto").click()
+        self.driver.find_element(By.ID, "stu_tab").click()
+        element = self.driver.find_element(By.ID, "stu_tab")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.CSS_SELECTOR, "body")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element, 0, 0).perform()
+        self.driver.find_element(By.CSS_SELECTOR, "tr:nth-child(1) > td:nth-child(2)").click()
+        self.driver.find_element(By.ID, "stu_table_toolbar_update").click()
+        element = self.driver.find_element(By.ID, "stu_table_toolbar_update")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.CSS_SELECTOR, "body")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element, 0, 0).perform()
+        self.driver.find_element(By.ID, "stu_table_toolbar_update_form_studentName").click()
+        self.driver.find_element(By.ID, "stu_table_toolbar_update_form_studentName").send_keys("小小的梦想🐷B1d")
+        self.driver.find_element(By.ID, "stu_table_toolbar_update_form_submit").click()
+        self.driver.find_element(By.CSS_SELECTOR, "#stu_table_toolbar_update_modal_dialog .ml-1 > .btn").click()
+        self.driver.find_element(By.NAME, "btSelectItem").click()
+        self.driver.find_element(By.ID, "stu_table_toolbar_add").click()
+        element = self.driver.find_element(By.ID, "stu_table_toolbar_add")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.CSS_SELECTOR, "body")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element, 0, 0).perform()
+        element = self.driver.find_element(By.ID, "stu_table_toolbar_add_form_studentName")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).click_and_hold().perform()
+        element = self.driver.find_element(By.ID, "stu_table_toolbar_add_form_studentName")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).perform()
+        element = self.driver.find_element(By.ID, "stu_table_toolbar_add_form_studentName")
+        actions = ActionChains(self.driver)
+        actions.move_to_element(element).release().perform()
+        self.driver.find_element(By.ID, "stu_table_toolbar_add_form_studentName").click()
+        self.driver.find_element(By.ID, "stu_table_toolbar_add_form_studentName").send_keys("dsd")
+        self.driver.find_element(By.ID, "stu_table_toolbar_add_form_idCard").click()
+        self.driver.find_element(By.ID, "stu_table_toolbar_add_form_idCard").send_keys("ds")
+        self.driver.find_element(By.ID, "stu_table_toolbar_add_form_studentEmail").click()
+        self.driver.find_element(By.ID, "stu_table_toolbar_add_form_studentEmail").send_keys("dsds")
+        self.driver.find_element(By.ID, "stu_table_toolbar_add_form_studentNo").click()
+        self.driver.find_element(By.ID, "stu_table_toolbar_add_form_studentNo").send_keys("dsd")
+        self.driver.find_element(By.CSS_SELECTOR, ".form-group:nth-child(4) .filter-option-inner-inner").click()
+        self.driver.find_element(By.CSS_SELECTOR, ".show > .inner > .dropdown-menu > li:nth-child(1) .text").click()
+        dropdown = self.driver.find_element(By.ID, "stu_table_toolbar_add_form_classes.id")
+        dropdown.find_element(By.XPATH, "//option[. = '幼儿园😂大班']").click()
+        self.driver.find_element(By.CSS_SELECTOR,
+                                 "#stu_table_toolbar_add_form > .form-group:nth-child(6) .filter-option-inner-inner").click()
+        self.driver.find_element(By.ID, "stu_table_toolbar_add_form_submit").click()
+        self.driver.find_element(By.LINK_TEXT, "3").click()
 
     @unittest.skip('skipping')
     def test_hao(self):
