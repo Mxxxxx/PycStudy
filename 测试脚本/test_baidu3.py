a183fd63eef42fab28fdd38712d9811c6f9eb4f5
孟鑫 <907008127@qq.com>
2020-09-09 22:44:50 +0800

    tj

--- 测试脚本/test_baidu3.py
+++ 测试脚本/test_baidu3.py
@@ -1,4 +1,6 @@
+import HTMLTestRunner
 import os
+import sys
 import time
 import unittest
 
@@ -9,7 +11,7 @@ class Baidu3(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "http://www.baidu.com"
+        self.base_url = "https://www.baidu.com"
 
     def test_baiduSearch(self):
         driver = self.driver
@@ -21,6 +23,8 @@ class Baidu3(unittest.TestCase):
             self.assertEqual("测试_百度", driver.title, msg="网页未打开")
         except:
             self.savescreenshot(driver, "baiduerror.png")
+        time.sleep(3)
+        driver.quit()
 
     # 生成测试截图
     def savescreenshot(self, driver, file_name):
@@ -29,3 +33,24 @@ class Baidu3(unittest.TestCase):
         now = time.strftime("%Y%m%d-%H%M%S", time.localtime(time.time()))
         driver.get_screenshot_as_file("./测试截图/" + now + file_name)
         time.sleep(3)
+
+# 测试报告
+# if __name__ == "__main__":
+#     # 当前文件所在得到文件夹的绝对路径
+#     curpath = sys.path[0]
+#     if not os.path.exists(curpath + '/测试报告'):
+#         os.makedirs(curpath + '/测试报告')
+#
+#     now = time.strftime("%Y-%m-%d-%H %M %S", time.localtime(time.time()))
+#     print("------------")
+#     print(time.time())
+#     print("------------")
+#     print(time.localtime(time.time()))
+#     print("------------")
+#     print(now)
+#
+#     filename = curpath + '/resultreport/' + now + 'resultreport.html'
+#     with open(filename, 'wb+') as fp:
+#         runner = HTMLTestRunner.HTMLTestRunner(stream=fp, title=u"测试报告", description=u"用例报告", verbosity=2)
+#         suite = creatsuit()
+#         runner.run(suite)
