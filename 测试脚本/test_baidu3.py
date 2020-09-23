1afbb88bccf86776cecfc81a452d77019cadb6f4
孟鑫 <907008127@qq.com>
2020-09-23 08:23:31 +0800

    tj

--- 测试脚本/test_baidu3.py
+++ 测试脚本/test_baidu3.py
@@ -11,21 +11,20 @@ class Baidu3(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Firefox()
         self.driver.implicitly_wait(30)
-        self.base_url = "https://www.baidu.com"
+        self.base_url = "http://47.92.97.206:8080/sis/public/index.html"
 
     def test_baiduSearch(self):
         driver = self.driver
         driver.get(self.base_url + '/')
-        driver.implicitly_wait(30)
-        driver.find_element_by_id("kw").send_keys("测试")
-        driver.find_element_by_id("su").click()
-        try:
-            self.assertEqual("测试_百度", driver.title, msg="网页未打开")
-        except:
-            self.savescreenshot(driver, "baiduerror.png")
-        time.sleep(3)
+        time.sleep(6)
+        # driver.find_element_by_id("kw").send_keys("测试")
+        # driver.find_element_by_id("su").click()
+        # try:
+        #     self.assertEqual("测试_百度", driver.title, msg="网页未打开")
+        # except:
+        #     self.savescreenshot(driver, "baiduerror.png")
+        # time.sleep(3)
         driver.quit()
-
     # 生成测试截图
     def savescreenshot(self, driver, file_name):
         if not os.path.exists('./测试截图'):
